7a47211ed14a985f340d15d3ac2902493c4f3e9c
Jithendhar123 <75047832+Jithendhar123@users.noreply.github.com>
2024-12-26 22:19:29 -0500

    Update settings.py

--- HabitVenture/settings.py
+++ HabitVenture/settings.py
@@ -129,13 +129,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = 'login'
-
-# AUTHENTICATION_BACKENDS = (
-#     'django.contrib.auth.backends.ModelBackend'
-# )
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_ = 'habittester01@gmail.com'
-# EMAIL_HOST_PASSWORD = 'Test@123'
